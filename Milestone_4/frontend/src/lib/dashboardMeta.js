1c8b5f2619a5bd27b4fe0fceddaf5ded8ebf15dc
bhargav-sai-kilaru <bhargavsaikilaru@gmail.com>
2026-04-02 18:31:20 +0530

    Create dashboardMeta.js

---
+++ Milestone_4/frontend/src/lib/dashboardMeta.js
@@ -0,0 +1,15 @@
+export const INPUT_FIELD_KEYS = [
+  'continent',
+  'education_of_employee',
+  'has_job_experience',
+  'requires_job_training',
+  'no_of_employees',
+  'yr_of_estab',
+  'region_of_employment',
+  'prevailing_wage',
+  'unit_of_wage',
+  'full_time_position',
+  'application_month',
+];
+
+export const VISUALIZATION_PANELS = ['Radial Gauge', 'Processing Trends', 'Continent-Wise Comparison'];
